6b6ea4affa5a52605a455b85b402eae062a613dd
Lina0220 <linuaman2017@gmail.com>
2023-03-24 11:25:05 +0300

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-
-"""
-    Method that determines the number of minmum operations given n characters
-"""
+"""Module finds the minimum number of operation"""
 
 
 def minOperations(n):
     """
-        A function that calculates the fewest number of operations
-        needed to give a result of exactly n H characters in a file
-        args: n: Number of characters to be displayed
-        return:
-               number of min operations
+    Method for minimum operations
+    Parameters:
+        n (Integer): The number to reach
+    Returns:
+        Integer: either 0 or the number of steps
     """
-
-    now = 1
-    start = 0
-    counter = 0
-    while now < n:
-        remainder = n - now
-        if (remainder % now == 0):
-            start = now
-            now += start
-            counter += 2
+    final = ''  # the final n number of chars reached
+    paste = 'H'  # the paste character
+    counter = 0  # the number of steps
+    if n == 1:
+        return 0
+    while len(final) < n:
+        # if nothing or only one is copied
+        if len(final) == 0 or len(final) == 1:
+            # copy the copied char on paste variable
+            final += paste
         else:
-            now += start
-            counter += 1
-    return 
+            # if the length of final is multiple of n
+            if n % len(final) == 0:
+                # copy all to the paste
+                paste = final
+                # increament b/c copy all step is used
+                counter += 1
+            # paste to the final both if and elif
+            final += paste
+        # add count as one step of paste is passed
+        counter += 1
+    # after looping if length of final is not equal to n
+    if len(final) != n:
+        return 0
+    return counter
