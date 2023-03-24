2f5d7c7e86c011dac384c0c43f388cd37e752887
Lina0220 <linuaman2017@gmail.com>
2023-03-24 10:34:29 +0300

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
-"""Script will unlock list of lists"""
+"""This module checks if the boxes can be unlocked
+from the values of other boxes
+"""
 
 
 def canUnlockAll(boxes):
-    """This function will take a list of lists and the content
-       of a list will unlock other lists
+    """This method checkes if all boxes are opened
+       Also opens locked boxes if possible
     """
-
-    keys = [0]
-    for key in keys:
-        for boxKey in boxes[key]:
-            if boxKey not in keys and boxKey < len(boxes):
-                keys.append(boxKey)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    size = len(boxes)
+    checker = []
+    # Initialize the array indexes to opened false and visited false
+    for i in range(size):
+        checker.append({"opened": False, "visited": False})
+    # First array shall be opened true
+    checker[0]["opened"] = True
+    # Done checkes if any box is opened and visited
+    done = False
+    # Visite the boxes when the boxes are opened but not visited
+    while(done is False):
+        done = True
+        # Try opening boxes based on the values inside the opened boxes
+        for i in range(size):
+            if (checker[i]['opened'] is True and
+                    checker[i]['visited'] is False):
+                for j in range(len(boxes[i])):
+                    if (boxes[i][j] < len(boxes)):
+                        checker[boxes[i][j]]['opened'] = True
+                checker[i]['visited'] = True
+        # Check if ther is any remaining box that is opened and not visited
+        for i in range(size):
+            if (checker[i]['opened'] is True and
+                    checker[i]['visited'] is False):
+                done = False
+    # Check if all the boxes have been opened
+    for i in range(size):
+        if (checker[i]['opened'] is False):
+            return False
+    return True
